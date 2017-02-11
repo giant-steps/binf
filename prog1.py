f6478432934efe5f29060c05f5c1c4addd82c23f
giant-steps <chrisdklocke@gmail.com>
2017-02-11 16:00:17 -0500

    progress on fasta_parse.py and prog1.py

--- prog1.py
+++ prog1.py
@@ -15,7 +15,23 @@ def overlap:
 
 ##main function definition
 def main:
-
+    fasta_dict = {}
+    with open(sys.argv[1], 'r') as input:
+        for line in input:  # this 'for' loop reads the multifasta input file, line by line, and stores the sequences in a dictionary,
+            line = line.rstrip()  # with the fasta identifiers as the dictionary keys
+            if line == '':
+                idstore = 'empty'
+                continue
+            elif line.startswith('>'):
+                idstore = line
+                continue
+            elif idstore.startswith('>'):           #this 'try, except' setup helps if there are returns interpreted in a multi-line DNA sequence
+                try:
+                    if fasta_dict[str(idstore[1:])] == 1:
+                        x = 'do nothing'
+                    fasta_dict[str(idstore[1:])] += line
+                except KeyError:
+                    fasta_dict[str(idstore[1:])] = line
 
 ###run main
 if __name__ == "__main__":
