0cb5e0184ddaa146ed59d700e91a8f40e8758e09
giant-steps <chrisdklocke@gmail.com>
2017-02-12 20:25:09 -0500

    started prog2.py

---
+++ prog2.py
@@ -0,0 +1,22 @@
+##this should solve "Rabbits and Recurrence Relations"
+
+## run with these arguments: >python prog2.py 'n-value' 'k-value' answers.txt
+
+##import statements
+import sys
+
+#function definitions
+def rabbits(n,k):
+
+
+##main function definition
+def main():
+    a = sys.argv[1]
+    b = sys.argv[2]
+    with open(sys.argv[3], 'w') as out:
+        out.append(rabbits(a,b))
+
+##run main function
+if __name__ == "__main__":
+    main()
+
