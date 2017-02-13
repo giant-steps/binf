9796280ae0870e9aa3a04e5907e1928bedffb63a
giant-steps <chrisdklocke@gmail.com>
2017-02-12 21:31:03 -0500

    prog2.py finished? fix cycle # issue...

--- prog2.py
+++ prog2.py
@@ -2,28 +2,32 @@
 
 ## run with these arguments: >python prog2.py 'n-value' 'k-value' answers.txt
 
+## it takes a month for rabbits to reach breeding maturity, ALSO takes a month for young rabbits to be born
+## so after 2 cycles pass, a given pair kicks in 'k' pairs of young rabbits per cycle
+
 ##import statements
 import sys
 
 #function definitions
 def rabbits(n,k):
-    herd = 1
     breed = 0
+    birth = 0
+    young = 1
     while n > 0:
         n -= 1
-        young = breed * k
-        breed = herd
-        herd = young + breed
+        birth = birth + breed
+        breed = young
+        young = birth * k
+        herd = young + breed + birth
 
     return herd
 
-
 ##main function definition
 def main():
-    a = sys.argv[1]
-    b = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
     with open(sys.argv[3], 'w') as out:
-        out.append(rabbits(a,b))
+        out.write(str(rabbits(a,b)))
 
 ##run main function
 if __name__ == "__main__":
