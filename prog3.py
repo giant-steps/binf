ddc8d2a92b745625d69d78ee811559ca768080cc
giant-steps <chrisdklocke@gmail.com>
2017-02-14 20:30:05 -0500

    prog3.py FINISHED

--- prog3.py
+++ prog3.py
@@ -5,11 +5,12 @@ import sys
 ## function definitions
 def mass_sum(translate, sequence):
     window = 0
-    total = 0
+    total = float(0)
     while window < len(sequence):
-        total += translate[sequence[window]]
+
+        total += float(translate[sequence[window]])
         window += 1
-    return total
+    return round(total, 10)
 
 ## main function definition
     ## use aa_monoisotopicmasstable.txt as 1st argument, input.txt as 2nd, answers.txt as 3rd
@@ -19,7 +20,8 @@ def main():
     with open(sys.argv[1], 'r') as table, open(sys.argv[2], 'r') as protein, open(sys.argv[3], 'w') as weight:
         for line in table:
             line = line.rstrip()
-            masses[line[0]] = line[1]
+            chunks = line.split()
+            masses[chunks[0]] = chunks[1]
         for line in protein:
             line = line.rstrip()
             aminoacid += str(line)
