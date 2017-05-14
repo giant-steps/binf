3be7a57a8ed03b7c679be651babdb07f35e0f939
giant-steps <chrisdklocke@gmail.com>
2017-05-13 20:38:16 -0400

    starting work on prog6.py

---
+++ .idea/prog6.py
@@ -0,0 +1,19 @@
+#this program will answer Rosalind problem "Enumerating Gene Orders"
+
+#import statements
+import sys
+
+
+#function definitions
+def factorial(n):
+    
+
+
+#main function
+def main():
+
+
+
+#execution of main function
+if __name__ == "__main__":
+    main()
