64ebf3db72461def0138f82a4fd9849bb363aef0
giant-steps <chrisdklocke@gmail.com>
2017-06-08 21:53:33 -0400

    work on prog7.py

--- prog7.py
+++ prog7.py
@@ -1,13 +1,21 @@
 #This program will answer Rosalind problem "Inferring mRNA from Protein"
 
 #import statements
-
+import sys
 
 #function definitions
-
+def i(inp):
+    decoder = {F:2,L:6,S:6,Y:2,C:2,W:1,P:4,H:2,Q:2,R:6,I:3,M:1,T:4,N:2,K:2,V:4,A:4,D:2,E:2,G:4}
+        ### there are 3 possible stop codons -- you will need one at the end (multiply by 3)
+    x = 3
+    for a in inp:
+        x = x * decoder[a]
+    return x
 
 #main function
-
+def main():
+    with str(open(sys.argv[1], 'r')) as seq:
+        print(i(seq))
 
 #execute main function
 if __name__ == "__main__":
