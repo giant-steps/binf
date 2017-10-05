104d5e60e02ee233d74e044e7b7f139c7cd522f2
giant-steps <chrisdklocke@gmail.com>
2017-10-04 22:26:08 -0400

    prog13.py starting

---
+++ prog13.py
@@ -0,0 +1,15 @@
+### sooo I think the best way to approach this is to use a sliding reading window along one of the strands
+#it will take the first letter, then check it in all the others, if it matches, then add the next letter, and so on until
+    #it fails --- this will give a common sequence -- BUT do this b/t 2 strands, not among all -- find significant overlap
+        #with one strand, save that superstring b/t the 2 and then move on to matching with others
+
+import sys
+
+def super():
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
+
