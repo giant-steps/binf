52bd467982b2355cc5408b31a247cdd6fa6ba07c
giant-steps <chrisdklocke@gmail.com>
2017-12-08 17:02:57 -0500

    progress on prog14.py

--- prog14.py
+++ prog14.py
@@ -3,7 +3,9 @@
 #practice creating a data structure that is a list of vectors
 #then read in data (possibly from excel or .csv files) & capture in this data structure, manipulate
 
-
+import numpy as np
+import csv
+import sys
 
 def up(st):
     st += 1
@@ -19,7 +21,33 @@ def main():
     f = list(map(up, e))
 
     tot = [a,b,c,d,e,f]
-    print(str(tot))
+    #print(str(tot))
+
+    x = np.array(tot)
+
+    #print(x)
+    #################
+    #test different items here
+
+    #y = np.full((500,8),0)
+
+    #print(str(y))
+    #####
+
+    #a, b, c, d, e, f, g, h, i = np.loadtxt(sys.argv[1] , skiprows=1, unpack=True)
+
+
+    reader = csv.reader(open(sys.argv[1]), delimiter=",")
+    x = list(reader)
+    result = np.array(x).astype("str")
+
+    print(str(result))
+
+
+
+
+
+#################
 
 if __name__ == "__main__":
     main()
